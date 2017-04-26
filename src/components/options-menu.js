fa7135245eff773a18853d19d92f3737ab25cb39
Chris Clifford <cclifford82@gmail.com>
2017-04-26 16:03:10 -0700

    reworks sidebar ui

--- src/components/options-menu.js
+++ src/components/options-menu.js
@@ -3,17 +3,17 @@ import React, { Component } from 'react';
 export default (props) => {
   if (props.menuOpen === true) {
     return (
-      <div className="collapse show" id="collapseExample">
-        <span className="text-primary" onClick={() => props.setStyleAttribute({backgroundColor: 'bg-inverse'})}>Black Background</span>
-        <span className="text-primary" onClick={() => props.setStyleAttribute({backgroundColor: ''})}>White Background</span>
-        <span className="text-primary" onClick={() => props.setStyleAttribute({backgroundColor: 'bg-primary'})}>Blue Background</span>
-        <span className="text-primary" onClick={() => props.setStyleAttribute({alignment: 'float-left'})}>Left Align</span>
-        <span className="text-primary" onClick={() => props.setStyleAttribute({alignment: 'float-right'})}>Right Align</span>
-        <span className="text-primary" onClick={() => props.setStyleAttribute({title: ''})}>Black Heading</span>
-        <span className="text-primary" onClick={() => props.setStyleAttribute({title: 'text-white'})}>White Heading</span>
-        <span className="text-primary" onClick={() => props.setStyleAttribute({text: 'text-muted'})}>Grey Text</span>
-        <span className="text-primary" onClick={() => props.setStyleAttribute({text: 'text-white'})}>White Text</span>
-      </div>
+      <ul className="collapse show menu-item" id="collapseExample">
+        <li className="text-menu-option" onClick={() => props.setStyleAttribute({backgroundColor: 'bg-inverse'})}>Black Background</li>
+        <li className="text-menu-option" onClick={() => props.setStyleAttribute({backgroundColor: ''})}>White Background</li>
+        <li className="text-menu-option" onClick={() => props.setStyleAttribute({backgroundColor: 'bg-primary'})}>Blue Background</li>
+        <li className="text-menu-option" onClick={() => props.setStyleAttribute({alignment: 'float-left'})}>Left Align</li>
+        <li className="text-menu-option" onClick={() => props.setStyleAttribute({alignment: 'float-right'})}>Right Align</li>
+        <li className="text-menu-option" onClick={() => props.setStyleAttribute({title: ''})}>Black Heading</li>
+        <li className="text-menu-option" onClick={() => props.setStyleAttribute({title: 'text-white'})}>White Heading</li>
+        <li className="text-menu-option" onClick={() => props.setStyleAttribute({text: 'text-muted'})}>Grey Text</li>
+        <li className="text-menu-option" onClick={() => props.setStyleAttribute({text: 'text-white'})}>White Text</li>
+      </ul>
     );
   } else {
     return <div></div>
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -11,7 +11,8 @@ class SideBar extends Component {
     this.state = {menuOpen1: false,
                   menuOpen2: false,
                   menuOpen3: false,
-                  menuOpen4: false};
+                  menuOpen4: false,
+                };
 
     this.toggleDropDown = this.toggleDropDown.bind(this);
     this.setStyleAttribute = this.setStyleAttribute.bind(this);
@@ -29,42 +30,30 @@ class SideBar extends Component {
     return (
       <div id="my-side-bar" className="side-bar">
         <span className="closebtn" onClick={this.props.closeNav}><i className="fa fa-times" aria-hidden="true"></i></span>
-          <div onClick={() => this.toggleDropDown({menuOpen1: true})} onMouseLeave={() => this.toggleDropDown({menuOpen1: false})}>
-            <span
-              onClick={() => this.props.selectLoopType({loopType: 'loop1'})}
-              data-toggle="collapse" href="#collapseExample"
-              aria-expanded="false" aria-controls="collapseExample">
-              Loop 1
-            </span>
-            <OptionsMenu menuOpen={this.state.menuOpen1} setStyleAttribute={this.setStyleAttribute} />
-          </div>
-          <div onClick={() => this.toggleDropDown({menuOpen2: true})} onMouseLeave={() => this.toggleDropDown({menuOpen2: false})}>
-            <span
-              onClick={() => this.props.selectLoopType({loopType: 'loop2'})}
-              data-toggle="collapse" href="#collapseExample"
-              aria-expanded="false" aria-controls="collapseExample">
-              Loop 2
-            </span>
-            <OptionsMenu menuOpen={this.state.menuOpen2} setStyleAttribute={this.setStyleAttribute} />
-          </div>
-          <div onClick={() => this.toggleDropDown({menuOpen3: true})} onMouseLeave={() => this.toggleDropDown({menuOpen3: false})}>
-            <span
-              onClick={() => this.props.selectLoopType({loopType: 'loop3'})}
-              data-toggle="collapse" href="#collapseExample"
-              aria-expanded="false" aria-controls="collapseExample">
-              Loop 3
-            </span>
-            <OptionsMenu menuOpen={this.state.menuOpen3} setStyleAttribute={this.setStyleAttribute} />
-          </div>
-          <div onClick={() => this.toggleDropDown({menuOpen4: true})} onMouseLeave={() => this.toggleDropDown({menuOpen4: false})}>
-            <span
-              onClick={() => this.props.selectLoopType({loopType: 'loop4'})}
-              data-toggle="collapse" href="#collapseExample"
-              aria-expanded="false" aria-controls="collapseExample">
-              Loop 4
-            </span>
-            <OptionsMenu menuOpen={this.state.menuOpen4} setStyleAttribute={this.setStyleAttribute} />
-          </div>
+        <div onClick={() => this.toggleDropDown((this.state.menuOpen1 ? {menuOpen1: false} : {menuOpen1: true}))}>
+          <span onClick={() => this.props.selectLoopType({loopType: 'loop1'})} className="menu-item">
+            Loop 1 <i className={(this.state.menuOpen1 ? "fa fa-chevron-down ml-3" : "fa fa-chevron-right ml-3")} aria-hidden="true"></i>
+          </span>
+        </div>
+        <OptionsMenu menuOpen={this.state.menuOpen1} setStyleAttribute={this.setStyleAttribute} />
+        <div onClick={() => this.toggleDropDown((this.state.menuOpen2 ? {menuOpen2: false} : {menuOpen2: true}))}>
+          <span onClick={() => this.props.selectLoopType({loopType: 'loop2'})} className="menu-item">
+            Loop 2 <i className={(this.state.menuOpen2 ? "fa fa-chevron-down ml-3" : "fa fa-chevron-right ml-3")} aria-hidden="true"></i>
+          </span>
+        </div>
+        <OptionsMenu menuOpen={this.state.menuOpen2} setStyleAttribute={this.setStyleAttribute} />
+        <div onClick={() => this.toggleDropDown((this.state.menuOpen3 ? {menuOpen3: false} : {menuOpen3: true}))}>
+          <span onClick={() => this.props.selectLoopType({loopType: 'loop3'})} className="menu-item">
+            Loop 3 <i className={(this.state.menuOpen3 ? "fa fa-chevron-down ml-3" : "fa fa-chevron-right ml-3")} aria-hidden="true"></i>
+          </span>
+        </div>
+        <OptionsMenu menuOpen={this.state.menuOpen3} setStyleAttribute={this.setStyleAttribute} />
+        <div onClick={() => this.toggleDropDown((this.state.menuOpen4 ? {menuOpen4: false} : {menuOpen4: true}))}>
+          <span onClick={() => this.props.selectLoopType({loopType: 'loop4'})} className="menu-item">
+            Loop 4 <i className={(this.state.menuOpen4 ? "fa fa-chevron-down ml-3" : "fa fa-chevron-right ml-3")} aria-hidden="true"></i>
+          </span>
+        </div>
+        <OptionsMenu menuOpen={this.state.menuOpen4} setStyleAttribute={this.setStyleAttribute} />
       </div>
     );
   }
